8aa6cfe1895cbb54364bd4acc8dc9914bf13d039
Calvin Seamons <calvinseamons35@gmail.com>
2023-11-17 00:32:23 -0700

    Changed target data to just pass or rush

--- THEMODEL.py
+++ THEMODEL.py
@@ -1,32 +1,74 @@
+import joblib
 import pandas as pd
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.neural_network import MLPClassifier
-from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from joblib import dump, load
+
 
 class THEMODEL:
     def __init__(self):
+        self.categorical_columns = None
         self.model = None
+        self.encoder = OneHotEncoder(sparse=False, handle_unknown='ignore')
+        self.label_encoder = LabelEncoder()
 
     def train(self, dataFrames):
-        X = dataFrames[0].drop(['Play Type', 'Id', 'Offense', 'Offense Conference'], axis=1)
-        y = dataFrames[0]['Play Type']
+        combined_df = pd.concat(dataFrames, ignore_index=True)
+
+        # Define play types to keep
+        keep_play_types = ['Rush', 'Pass', 'Rushing Touchdown', 'Pass Reception', 'Pass Incompletion',
+                           'Passing Touchdown', 'Pass Interception Return']
+
+        # Filter the DataFrame
+        combined_df = combined_df[combined_df['Play Type'].isin(keep_play_types)]
+
+        play_type_mapping = {
+            'Rushing Touchdown': 'Rush',
+            'Pass Reception': 'Pass',
+            'Pass Incompletion': 'Pass',
+            'Passing Touchdown': 'Pass',
+            'Pass Interception Return': 'Pass'
+        }
+
+        combined_df = combined_df.replace(play_type_mapping)
+
+        X = combined_df.drop(['Play Type', 'Id', 'Offense', 'Offense Conference'], axis=1)
+        y = combined_df['Play Type']
+
+        self.categorical_columns = X.select_dtypes(include=['object', 'category']).columns.tolist()
+
+        for col in self.categorical_columns:
+            X[col] = X[col].astype(str)
 
         # Handle NaN values
         X = X.fillna(0)
         y = y.fillna(0)
 
-        # One hot encode all of the categorical data
+        # One hot encode all the categorical data
         le = LabelEncoder()
-        X = pd.get_dummies(X)
+        self.encoder.fit(X[self.categorical_columns])
+        X_encoded = self.encoder.transform(X[self.categorical_columns])
+
         y = le.fit_transform(y)
 
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+        self.label_encoder = le
 
-        self.perform_grid_search(X_train, y_train)
+        X_train, X_test, y_train, y_test = train_test_split(X_encoded, y, test_size=0.3)
+
+        # self.perform_grid_search(X_train, y_train)
+
+        self.model = MLPClassifier(activation='logistic', alpha=1e-05, beta_1=0.95,
+                                   epsilon=1e-08, hidden_layer_sizes=100,
+                                   learning_rate='constant', learning_rate_init=0.05,
+                                   max_iter=10000, momentum=0.9, random_state=1,
+                                   solver='adam')
 
         self.model.fit(X_train, y_train)
-        print(self.model.score(X_test, y_test))
 
+        joblib.dump({'model': self.model, 'encoder': self.encoder, 'categorical_columns': self.categorical_columns, 'label_encoder': self.label_encoder}, 'trained_model.joblib')
+
+        print(self.model.score(X_test, y_test))
 
     def perform_grid_search(self, X, y):
         parameter_space = {
@@ -54,4 +96,6 @@ class THEMODEL:
         self.model = clf.best_estimator_
 
     def predict(self, X):
+        X = X.reindex(columns=self.columns).fillna(0)
+        self.model = load('model.joblib')
         return self.model.predict(X)
--- modelTesting.py
+++ modelTesting.py
@@ -1,15 +1,61 @@
-import warnings
-warnings.filterwarnings('ignore')
-warnings.simplefilter(action='ignore', category=FutureWarning)
+import joblib
+import pandas as pd
+from sklearn.metrics import accuracy_score
 
 from THEMODEL import THEMODEL
 from readData import readData
+from joblib import dump, load
+import warnings
+
+warnings.filterwarnings('ignore')
+warnings.simplefilter(action='ignore', category=FutureWarning)
 
 
 def main():
-    model = THEMODEL()
     dataFrames = readData()
-    model.train(dataFrames)
+    print("Preparing for prediction...")
+    # Train the model
+    # model = THEMODEL()
+    # model.train(dataFrames)
+
+    # Load the trained model (Current Version already saved)
+    saved_data = joblib.load('trained_model.joblib')
+    trained_model = saved_data['model']
+    encoder = saved_data['encoder']
+    categorical_columns = saved_data['categorical_columns']
+    label_encoder = saved_data['label_encoder']
+
+    ###########################################################
+    ############## PREDICTING ON NEW DATA #####################
+    ######## HIGHEST ACCURACY SO FAR: 0.6385542168674698#######
+    ###########################################################
+
+    # Select the DataFrame for prediction
+    X_new = dataFrames[7].drop(['Play Type', 'Id', 'Offense', 'Offense Conference'], axis=1)
+    keep_play_types = ['Rush', 'Pass', 'Rushing Touchdown', 'Pass Reception', 'Pass Incompletion',
+                       'Passing Touchdown', 'Pass Interception Return']
+
+    play_type_mapping = {
+        'Rushing Touchdown': 'Rush',
+        'Pass Reception': 'Pass',
+        'Pass Incompletion': 'Pass',
+        'Passing Touchdown': 'Pass',
+        'Pass Interception Return': 'Pass'
+    }
+
+    # Handle missing values
+    X_new = X_new.fillna(0)
+
+    X_new_encoded = encoder.transform(X_new[categorical_columns])
+
+    predictions = trained_model.predict(X_new_encoded)
+    predictions = label_encoder.inverse_transform(predictions)
+    # print(predictions)
+
+    y = dataFrames[7]['Play Type']
+    y = y.fillna(0)
+    y = y[y.isin(keep_play_types)].replace(play_type_mapping)
+    # print(y)
 
 
 if __name__ == '__main__':
